636e39ad326912ecb414cc434d736b3adee149dd
Larry-the-Pig <68445204+Larry-the-Pig@users.noreply.github.com>
2021-11-16 10:34:04 -0700

    Delete school/string programms directory

--- school/string programms/search.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById("input");
-
-document.addEventListener("keydown", keyDown);
-
-function keyDown(evt) {
-    if (evt.keyCode == 13 && input.value != "") {
-        window.location.href = "results.html?q=" + input.value;
-    }
-}
