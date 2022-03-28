16e03af0c1785ae4cdfbafeec01f952b947b0565
Larry-the-Pig <68445204+Larry-the-Pig@users.noreply.github.com>
2022-03-28 09:14:37 -0600

    Add files via upload

---
+++ school/sent types/index.js
@@ -0,0 +1,54 @@
+const form = document.querySelector("form");
+const header = document.getElementById("sentence");
+const correctSfx = new Audio('res/correct.ogg');
+const incorrectSfx = new Audio('res/incorrect.ogg');
+
+let currentSent = 0;
+let currentSkill = 1;
+
+function generateSentence(skill) {
+    currentSent = Math.floor(Math.random() * skills[skill].length)
+
+    //sets the text content of the page header to the current sentence
+    header.textContent = skills[skill][currentSent].sentence + " (Current Skill: " + currentSkill + ")";
+}
+
+function checkAnswer(input) {
+    if (input == skills[currentSkill][currentSent].answer) {
+        //sets text to green and generates a new sentence
+        correct()
+    } else {
+        //flashes red and plays the incorrect sound effect
+        incorrect()
+    }
+}
+
+function correct() {
+    header.style.color = "lime";
+    correctSfx.play();
+    setTimeout(() => {
+        currentSkill++;
+        if (currentSkill > 3) currentSkill = 3;
+        generateSentence(currentSkill);
+        header.style.color = "white";
+    }, 500);
+}
+
+function incorrect() {
+    header.style.color = "red";
+    incorrectSfx.play();
+    setTimeout(() => {
+        currentSkill--;
+        if (currentSkill < 1) currentSkill = 1;
+        generateSentence(currentSkill);
+        header.style.color = "white";
+    }, 500);
+}
+
+form.onsubmit = function(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    checkAnswer(formData.get('type'))
+}
+
+generateSentence(currentSkill);
---
+++ school/sent types/sentences.js
@@ -0,0 +1,45 @@
+const skills = 
+{
+    1: [
+        {
+            "sentence": "I walked my dog.",
+            "answer": "simple"
+        },
+        {
+            "sentence": "I bought mayo at the store.",
+            "answer": "simple"
+        }
+    ],
+    2: [
+        {
+            "sentence": "I walk my dog, and my brother walks his.",
+            "answer": "compound"
+        },
+        {
+            "sentence": "Several others in our class have submitted their projects.",
+            "answer": "simple"
+        },
+        {
+            "sentence": "Although she didn't study, she still got an acceptable grade.",
+            "answer": "complex"
+        }
+    ],
+    3: [
+        {
+            "sentence": "The quick brown fox, who is really energetic, jumped over the lazy dog.",
+            "answer": "complex"
+        },
+        {
+            "sentence": "Jennifer, who works in IT, loves her job.",
+            "answer": "complex"
+        },
+        {
+            "sentence": "Although it was sunny an hour ago, it was raining cats and dogs outside.",
+            "answer": "complex"
+        },
+        {
+            "sentence": "Kangaroos, koalas, and opossums are all marsupials.",
+            "answer": "simple"
+        },
+    ]
+}
